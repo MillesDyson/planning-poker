2cd02ff09425d931fcaa7cca24e5c1ced4d242c9
Milles Dyson Schroeder <milles.schroeder@gmail.com>
2022-07-06 23:40:39 -0300

    build: Adding Css Modules Plugin for Typescript

---
+++ app/javascript/components/layouts/header/AppHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.scss';
+
+const AppHeader: React.FC = () => {
+  return (
+    <div className={classes.item}>
+      teste
+    </div>
+  );
+}
+
+export default AppHeader;
